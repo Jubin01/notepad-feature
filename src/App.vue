36ac1c8721d7cd10b7876e69e25756b79916d88b
jubin thomas <athom130@adient.com>
2024-06-15 12:52:09 +0530

    ##notepad

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="notes">
+    <h3>Notes</h3>
+    <div v-for="(value, index) in notes" :key="index">
+      <h6>{{ value.date }}, source: <a :href=value.source target="_blank">
+          <img :alt=value.source :src=value.icon height="16" :title=value.source /></a></h6>
+      <h4>{{ value.note }} </h4>
+      <br />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import notes from "../public/assets/notes.json";
+export default {
+  name: 'Notes',
+  data: () => ({
+    notes: []
+  }),
+  mounted() {
+    this.notes = notes.notes;
+  }
+};
+
+</script>
+<!-- <style>
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+#root {
+  font-family: "Open Sans", sans-serif;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  /* overflow: hidden; */
+  background-color: #E1E2E1 !important;
+}
+</style> -->
